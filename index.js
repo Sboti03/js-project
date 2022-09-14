43b87ee8a37dfcbdcfd35c9fc00a081dcd1e0022
avpboy <somogyibotond03@gmail.com>
2022-09-14 12:29:01 +0200

    lodash

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 import { random } from './matek.js'
+import { LOREM, NAME } from './texts/unique.js'
+import lodash from 'lodash'
 
 console.log(random(5,10))
-
-
-
-
-
+console.log(NAME + ' ' +  LOREM)
 
