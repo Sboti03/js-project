2fa8336c81d62409b6a04475ed754c0e37030c38
avpboy <somogyibotond03@gmail.com>
2022-09-14 14:23:29 +0200

    bongeszo

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import _ from "lodash"
 import { random } from './matek.js'
 import { LOREM, NAME } from './texts/unique.js'
-import 'bootstrap/dist/css/bootstrap.css'
-import bs from 'bootstrap'
+
 
 
 console.log(random(5,10))
@@ -15,3 +14,10 @@ let t3 = [2]
 let combine = _.concat(t1, t2, t3)
 
 console.log(combine)
+
+function katt(){
+    alert('Rányomtál erre a gombra')
+}
+
+
+document.getElementById('gomb').addEventListener('click', katt)
