a31acee500f162308a725a8b144bb78db0e87eb2
avpboy <somogyibotond03@gmail.com>
2022-09-14 12:45:04 +0200

    combine

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
+import _ from "lodash"
 import { random } from './matek.js'
 import { LOREM, NAME } from './texts/unique.js'
-import lodash from 'lodash'
 
 console.log(random(5,10))
 console.log(NAME + ' ' +  LOREM)
 
+let t1 = [1, 2, 3]
+let t2 = [ 123, 32, 32]
+let t3 = [2]
+
+let combine = _.concat(t1, t2, t3)
+
+console.log(combine)
